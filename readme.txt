d62d5d926f7ea2e1028fb163edd0f88fe2af8d21
WP Wham <support@wpwham.com>
2018-10-01 18:18:31 -0700

    initial commit (v2.4.3)

---
+++ readme.txt
@@ -0,0 +1,156 @@
+=== Custom Price Labels for WooCommerce ===
+Contributors: wpwham
+Tags: woocommerce
+Requires at least: 4.1
+Tested up to: 4.9
+Stable tag: 2.4.3
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Create any custom price label for any WooCommerce product.
+
+== Description ==
+
+WooCommerce Custom Price Labels plugin lets you add any price label to any WooCommerce product.
+
+Labels can be set **globally** for all products, or locally on **per product** basis.
+
+Optionally you can select if you want to override global price labels with per product labels (if set), or combine global and local labels.
+
+You can also use included **bulk price labels editor tool** to modify multiple individual products labels.
+
+Labels can be added at different **positions**:
+
+* before the price
+* after the price
+* instead of the price
+
+Each label can be customized to be shown or hidden **by page type**:
+
+* home page
+* products page (i.e. archives)
+* single product page
+* related products
+* all pages
+* cart page only
+
+Additionally for **variable products** labels can be customized to be shown or hidden for:
+
+* main price
+* all variations
+
+You can also show label for selected **user roles** only, or hide label for selected user roles.
+
+= Feedback =
+* We are open to your suggestions and feedback - Thank you for using or trying out one of our plugins!
+* Drop us a line at [www.wpwham.com](http://www.wpwham.com).
+
+= More =
+* Visit the [Custom Price Labels for WooCommerce plugin page](https://wpfactory.com/item/custom-price-labels-for-woocommerce-plugin/).
+
+== Installation ==
+
+1. Upload the entire plugin folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the "Plugins" menu in WordPress.
+3. Go to "WooCommerce > Settings > Custom Price Labels".
+
+== Frequently Asked Questions ==
+
+= What are the differences between free and Pro versions? =
+
+Free vs Pro comparison table is [here](https://wpfactory.com/item/custom-price-labels-for-woocommerce-plugin/#free_vs_pro).
+
+== Screenshots ==
+
+1. Custom Price Labels for WooCommerce - General Dashboard.
+2. Custom Price Labels for WooCommerce - Global Labels (all products).
+3. Custom Price Labels for WooCommerce - Global Labels - Visibility Options.
+4. Custom Price Labels for WooCommerce - Per Product Labels Options.
+5. Custom Price Labels for WooCommerce - Per Product Labels (product's edit page).
+6. Custom Price Labels for WooCommerce - Per Product Labels - Custom Price Label Bulk Editor Tool.
+
+== Changelog ==
+
+= 2.4.3 - 11/09/2018 =
+* Dev - Using `wc_help_tip()` function for tooltips.
+* Dev - Code refactoring.
+
+= 2.4.2 - 10/09/2018 =
+* Dev - "Contributors" and "Author URI" updated.
+
+= 2.4.1 - 05/08/2018 =
+* Dev - Admin settings descriptions updated.
+* Dev - Plugin link updated.
+
+= 2.4.0 - 19/04/2017 =
+* Dev - WooCommerce v3.x.x compatibility - `$product->id`.
+* Dev - WooCommerce v3.x.x compatibility - `woocommerce_get_variation_price_html` filter.
+* Dev - Visibility - "All pages" replaced with "All pages (except homepage)".
+* Dev - Custom Price Label Bulk Editor Tool - Restyled.
+* Dev - readme.txt updated: screenshots, faq etc.
+* Dev - Filter rewritten.
+* Dev - `coder.fm` links changed to `wpcodefactory.com`.
+
+= 2.3.0 - 23/03/2017 =
+* Dev - Custom Price Label Bulk Editor Tool - `WP_Query` optimized to return `ids` only.
+* Dev - Per Product Custom Price Labels - "Disable Options" option added.
+* Dev - Per Product Custom Price Labels - "Hide on" checkboxes replaced by multiple select (dropdown box).
+* Dev - Per Product Custom Price Labels - "Wrap Per Product Custom Price Labels" options added.
+* Dev - General - "Disable custom price labels for search bots" option added.
+* Dev - Settings divided to separate sections. "Reset Section Settings" option added.
+* Dev - "Enable section" option added (to both global and per product price labels).
+* Dev - "Show on" options added (to both global and per product price labels).
+* Dev - "Hide on single product page except main product price (e.g. related)" option added (to both global and per product price labels).
+* Dev - Language (POT) file updated.
+* Dev - Code refactoring.
+* Dev - Descriptions etc. updated.
+
+= 2.2.1 - 08/03/2017 =
+* Dev - Global Custom Price Labels - "Visibility on Site" options added.
+* Dev - Language (POT) file updated.
+
+= 2.2.0 - 01/02/2017 =
+* Dev - User Roles to Hide/Show options added.
+* Dev - Global Custom Price Labels - Textarea settings fields replaced with custom textarea fields.
+* Dev - Language (POT) file updated.
+* Dev - Minor code refactoring.
+* Dev - readme.txt updated.
+
+= 2.1.2 - 26/01/2017 =
+* Dev - "Override Global Price Labels with Per Product Labels" option added.
+
+= 2.1.1 - 27/12/2016 =
+* Dev - Version system added.
+* Fix - `load_plugin_textdomain()` function moved from `init` hook to constructor.
+* Dev - Language (POT) file updated.
+* Dev - readme.txt updated.
+
+= 2.1.0 - 24/05/2016 =
+* Dev - Translations - POT file uploaded. "Text Domain" and "Domain Path" added to the plugin's header.
+* Dev - Multisite support added.
+* Dev - "Custom Price Label Bulk Editor Tool" added.
+* Dev - Plugin renamed from "WooCommerce Custom Price Label" to "Custom Price Labels for WooCommerce".
+
+= 2.0.1 - 05/08/2015 =
+* Dev - Description in readme.txt extended.
+* Dev - Description added to General settings tab.
+
+= 2.0.0 - 05/08/2015 =
+* Dev - Global labels added.
+* Dev - Per product options: Hide on all pages, Hide on cart page only, Hide for main (variable) price, Hide for all variations.
+* Dev - Per product options: Before the price unlocked.
+* Dev - Major code refactoring. Settings are moved to "WooCommerce > Settings > Custom Price Label".
+
+= 1.0.2 =
+* Variable products bug fixed
+
+= 1.0.1 =
+* Minor bug fixed
+
+= 1.0.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+This is the first release of the plugin.
---
+++ woocommerce-custom-price-label.php
@@ -0,0 +1,199 @@
+<?php
+/*
+Plugin Name: Custom Price Labels for WooCommerce
+Plugin URI: https://wpfactory.com/item/custom-price-labels-for-woocommerce-plugin/
+Description: Create any custom price label for any WooCommerce product.
+Version: 2.4.3
+Author: Algoritmika Ltd
+Author URI: http://www.wpwham.com
+Text Domain: woocommerce-custom-price-label
+Domain Path: /langs
+Copyright: © 2018 Algoritmika Ltd.
+WC tested up to: 3.4
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Check if WooCommerce is active
+$plugin = 'woocommerce/woocommerce.php';
+if (
+	! in_array( $plugin, apply_filters( 'active_plugins', get_option( 'active_plugins', array() ) ) ) &&
+	! ( is_multisite() && array_key_exists( $plugin, get_site_option( 'active_sitewide_plugins', array() ) ) )
+) return;
+
+if ( 'woocommerce-custom-price-label.php' === basename( __FILE__ ) ) {
+	// Check if Pro is active, if so then return
+	$plugin = 'woocommerce-custom-price-label-pro/woocommerce-custom-price-label-pro.php';
+	if (
+		in_array( $plugin, apply_filters( 'active_plugins', get_option( 'active_plugins', array() ) ) ) ||
+		( is_multisite() && array_key_exists( $plugin, get_site_option( 'active_sitewide_plugins', array() ) ) )
+	) return;
+}
+
+if ( ! class_exists( 'Woocommerce_Custom_Price_Label' ) ) :
+
+/**
+ * Main Woocommerce_Custom_Price_Label Class
+ *
+ * @class   Woocommerce_Custom_Price_Label
+ * @version 2.4.3
+ */
+final class Woocommerce_Custom_Price_Label {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var   string
+	 * @since 2.1.1
+	 */
+	public $version = '2.4.3';
+
+	/**
+	 * @var Woocommerce_Custom_Price_Label The single instance of the class
+	 */
+	protected static $_instance = null;
+
+	/**
+	 * Main Woocommerce_Custom_Price_Label Instance
+	 *
+	 * Ensures only one instance of Woocommerce_Custom_Price_Label is loaded or can be loaded.
+	 *
+	 * @version 2.2.0
+	 * @static
+	 * @return  Woocommerce_Custom_Price_Label - Main instance
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Woocommerce_Custom_Price_Label Constructor.
+	 *
+	 * @access  public
+	 * @version 2.4.3
+	 */
+	function __construct() {
+
+		// Set up localisation
+		load_plugin_textdomain( 'woocommerce-custom-price-label', false, dirname( plugin_basename( __FILE__ ) ) . '/langs/' );
+
+		// Include required files
+		$this->includes();
+
+		// Admin
+		if ( is_admin() ) {
+			add_filter( 'woocommerce_get_settings_pages', array( $this, 'add_woocommerce_settings_tab' ) );
+			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'action_links' ) );
+			// Settings
+			require_once( 'includes/admin/class-wc-custom-price-label-settings-section.php' );
+			$this->settings = array();
+			$this->settings['general'] = require_once( 'includes/admin/class-wc-custom-price-label-settings-general.php' );
+			$this->settings['global']  = require_once( 'includes/admin/class-wc-custom-price-label-settings-global.php' );
+			$this->settings['local']   = require_once( 'includes/admin/class-wc-custom-price-label-settings-local.php' );
+			if ( get_option( 'alg_wc_custom_price_label_version', '' ) !== $this->version ) {
+				add_action( 'admin_init', array( $this, 'version_updated' ) );
+			}
+			// Per product settings
+			require_once( 'includes/admin/class-wc-custom-price-label-settings-per-product.php' );
+			// Bulk editor tool
+			require_once( 'includes/admin/class-wc-custom-price-label-bulk-editor-tool.php' );
+		}
+	}
+
+	/**
+	 * Show action links on the plugin screen.
+	 *
+	 * @version 2.4.0
+	 * @param   mixed $links
+	 * @return  array
+	 */
+	function action_links( $links ) {
+		$custom_links = array();
+		$custom_links[] = '<a href="' . admin_url( 'admin.php?page=wc-settings&tab=custom_price_label' ) . '">' . __( 'Settings', 'woocommerce' ) . '</a>';
+		if ( 'woocommerce-custom-price-label.php' === basename( __FILE__ ) ) {
+			$custom_links[] = '<a href="' . esc_url( 'https://wpfactory.com/item/custom-price-labels-for-woocommerce-plugin/' ) . '">' .
+				__( 'Unlock all', 'woocommerce-custom-price-label' ) . '</a>';
+		}
+		return array_merge( $custom_links, $links );
+	}
+
+	/**
+	 * Include required core files used in admin and on the frontend.
+	 *
+	 * @version 2.4.3
+	 */
+	function includes() {
+		// Functions
+		require_once( 'includes/wc-custom-price-label-functions.php' );
+		// Core
+		require_once( 'includes/class-wc-custom-price-label.php' );
+	}
+
+	/**
+	 * version_updated.
+	 *
+	 * @version 2.4.3
+	 * @since   2.4.3
+	 */
+	function version_updated() {
+		foreach ( $this->settings as $section ) {
+			foreach ( $section->get_settings() as $value ) {
+				if ( isset( $value['default'] ) && isset( $value['id'] ) ) {
+					$autoload = isset( $value['autoload'] ) ? ( bool ) $value['autoload'] : true;
+					add_option( $value['id'], $value['default'], '', ( $autoload ? 'yes' : 'no' ) );
+				}
+			}
+		}
+		update_option( 'alg_wc_custom_price_label_version', $this->version );
+	}
+
+	/**
+	 * Add Woocommerce settings tab to WooCommerce settings.
+	 *
+	 * @version 2.4.3
+	 */
+	function add_woocommerce_settings_tab( $settings ) {
+		$settings[] = require_once( 'includes/admin/class-wc-settings-custom-price-label.php' );
+		return $settings;
+	}
+
+	/**
+	 * Get the plugin url.
+	 *
+	 * @return string
+	 */
+	function plugin_url() {
+		return untrailingslashit( plugin_dir_url( __FILE__ ) );
+	}
+
+	/**
+	 * Get the plugin path.
+	 *
+	 * @return string
+	 */
+	function plugin_path() {
+		return untrailingslashit( plugin_dir_path( __FILE__ ) );
+	}
+}
+
+endif;
+
+if ( ! function_exists( 'WCCPL' ) ) {
+	/**
+	 * Returns the main instance of Woocommerce_Custom_Price_Label to prevent the need to use globals.
+	 *
+	 * @version 2.1.0
+	 * @return  Woocommerce_Custom_Price_Label
+	 */
+	function WCCPL() {
+		return Woocommerce_Custom_Price_Label::instance();
+	}
+}
+
+WCCPL();
